cb8a45ee4a058028a4e7e4e8161d3d59fa59f4de
ibrahem khaled <xx.vvv48@gmail.com>
2025-10-11 20:55:40 +0300

    feat: Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/smart-letter-guide/',
   server: {
     allowedHosts: [
       // allow all urls
