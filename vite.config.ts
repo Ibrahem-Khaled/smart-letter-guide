f52f954f7dbd145157904fae36507b17fc3792d7
ibrahem khaled <xx.vvv48@gmail.com>
2025-10-11 23:14:43 +0300

    fix: Update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/smart-letter-guide/',
+  base: '/',
   server: {
     allowedHosts: [
       // allow all urls
